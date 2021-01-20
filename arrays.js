b5c9f67c432f90c6bdc215975040d3db07938212
Cara M <caramaldonado@gmail.com>
2021-01-20 17:35:56 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
+
+function addElementToBeginningOfArray (array,element){
+var newArray = [element,...array]
+return newArray
+
+
+}
+
+function destructivelyAddElementToBeginningOfArray (array, element) {
+
+array.unshift(element)
+return array
+}
+
+function addElementToEndOfArray (array,element){
+var newArray =[...array,element]
+return newArray
+}
+
+function destructivelyAddElementToEndOfArray (array,element){
+  array.push (element)
+  return array
+}
+
+function accessElementInArray (array,index){
+return array [2]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray (array, element){
+  array.shift(element)
+  return array
+}
+
+function removeElementFromBeginningOfArray (array, element){
+var newArray = [array,element]
+  newArray = array.slice(1)
+  return newArray
+}
+function destructivelyRemoveElementFromEndOfArray (array,element){
+  
+}
